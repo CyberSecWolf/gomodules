10429fb5229d622ea909d5799f6ec7a0cdeca728
CyberSecWolf <cybersecuritywolf@protonmail.com>
2023-09-07 14:05:34 +0200

    utils added

---
+++ utils.go
@@ -0,0 +1,24 @@
+package drake
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Input(str string) (string, error) { // requirements: bufio, fmt, os, strings
+	var user_input string
+
+	reader := bufio.NewReader(os.Stdin) // Creates a new reader to read from standard input
+
+	fmt.Print(str) // Prompts the user for input
+
+	user_input, err := reader.ReadString('\n') // Read the input from the user
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(user_input), err
+}
+
